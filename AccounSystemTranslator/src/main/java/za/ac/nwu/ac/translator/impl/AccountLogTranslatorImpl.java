bec2b9e8ab3b2b72696a576452734fe6f3e86d6b
Bullets4Breakfast <ruben.erasmus.re@gmail.com>
2021-10-06 21:14:19 +0200

    custom Query

---
+++ AccounSystemTranslator/src/main/java/za/ac/nwu/ac/translator/impl/AccountLogTranslatorImpl.java
@@ -0,0 +1,35 @@
+package za.ac.nwu.ac.translator.impl;
+
+
+import org.springframework.stereotype.Component;
+import za.ac.nwu.ac.domain.persistence.AccountLog;
+import za.ac.nwu.ac.repo.persistence.AccountLogRepository;
+import za.ac.nwu.ac.translator.AccountLogTranslator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class AccountLogTranslatorImpl implements AccountLogTranslator {
+
+    private AccountLogRepository repo;
+
+    public AccountLogTranslatorImpl(AccountLogRepository accountLogRepository){
+        this.repo = accountLogRepository;
+    }
+
+    @Override
+    public AccountLog save(AccountLog accountLog) {
+        return repo.save(accountLog);
+    }
+
+    @Override
+    public List<AccountLog> getAllAccountLogs(){
+        List<AccountLog> accountLogs;
+
+        accountLogs = new ArrayList<>(repo.findAll());
+        return accountLogs;
+    }
+
+
+}
