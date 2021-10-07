26fe737213df5e6d49400e9c142cbd613b02a43f
Bullets4Breakfast <ruben.erasmus.re@gmail.com>
2021-10-07 18:05:08 +0200

    Account Link

--- AccounSystemTranslator/src/main/java/za/ac/nwu/ac/translator/AccountTypeTranslator.java
+++ AccounSystemTranslator/src/main/java/za/ac/nwu/ac/translator/AccountTypeTranslator.java
@@ -9,8 +9,8 @@ public interface AccountTypeTranslator {
     AccountTypeDto create(AccountTypeDto accountType);
 
    AccountTypeDto getAccountTypeByMnemonicNativeQuery(String mnemonic);
-//
-//    AccountTypeDto getAccountTypeByMnemonic(String mnemonic);
+
+    AccountTypeDto getAccountTypeByMnemonic(String mnemonic);
 //
 //    AccountTypeDto getAccountTypeDtoByMnemonic (String mnemonic);
 }
